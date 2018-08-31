d2a3fa1c85318941c5ba7337da58410a12c452c2
Suranart Niamcome <suranartnc@gmail.com>
2018-08-31 18:17:56 +0700

    Add babel plugin module resolver

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,19 @@
-const { compose } = require('react-app-rewired')
+const { compose, injectBabelPlugin } = require('react-app-rewired')
 const { createEmotionRewire } = require('react-app-rewire-emotion')
 
 module.exports = function override(config, env) {
+  config = injectBabelPlugin(
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@pages': './src/pages'
+        }
+      }
+    ],
+    config
+  )
+
   const rewires = compose(createEmotionRewire({ inline: true }))
   return rewires(config, env)
 }
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-import Home from './pages/Home'
-import About from './pages/About'
+import Home from '@pages/Home'
+import About from '@pages/About'
 
 export default function App() {
   return (
