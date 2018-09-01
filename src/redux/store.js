af7c3f231b74bdba41fda4f5584f284ba036eb2e
SuranartN <suranartnc@gmail.com>
2018-09-01 18:35:48 +0700

    Remove some values

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,12 +11,12 @@ import { reduxFirestore } from 'redux-firestore'
 import rootReducer from './reducer'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmLWUVd3nRKn0ZNmBF-m955_fGRsaNQyQ',
-  authDomain: 'pwa-starter-kit-b97bc.firebaseapp.com',
-  databaseURL: 'https://pwa-starter-kit-b97bc.firebaseio.com',
-  projectId: 'pwa-starter-kit-b97bc',
-  storageBucket: 'pwa-starter-kit-b97bc.appspot.com',
-  messagingSenderId: '516954074362'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 const rrfConfig = {
