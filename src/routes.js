f56c05e2565b9148a29cc5f8441c0cea0dd57748
SuranartN <suranartnc@gmail.com>
2018-09-01 12:46:46 +0700

    Add login page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
+import Login from '@pages/Login'
 import Home from '@pages/Home'
 import About from '@pages/About'
 
 export default [
   {
     path: '/',
+    exact: true,
+    component: Login
+  },
+  {
+    path: '/home',
     exact: true,
     component: Home
   },
